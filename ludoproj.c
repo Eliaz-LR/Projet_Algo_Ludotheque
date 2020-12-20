92d26018e38ab0aa698c83d78bb05f63f622f0ff
Eliouz <47760515+Eliaz-LR@users.noreply.github.com>
2020-12-20 12:38:14 +0100

    creation fonctions

--- ludoproj.c
+++ ludoproj.c
@@ -1,2 +1,1 @@
 #include "ludoproj.h"
-short
--- ludoproj.h
+++ ludoproj.h
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void filesLoad();
+
+void affJeux();
+
+void affEmp();
+
+void affRes();
+
+void addEmpRes();
+
+void retourJeux();
+
+void anuRes();
+
+void saveFiles();
